187cead30fe84bfc3b21d8b6bb00a6dd452477f1
Hounter139 <davidcr139@hotmail.com>
2021-02-26 12:45:24 -0500

    fini la

--- src/composants/Entete.jsx
+++ src/composants/Entete.jsx
@@ -1,10 +1,11 @@
 import avatar from '../images/avatar.png';
+import Avatar from '@material-ui/core/Avatar';
 
 export default function Entete() {
   return (
     <header className="Entete">
       <div className="logo">Signets</div>
-      <div className="avatar">Camille Semaan <img src={avatar} alt="Camille Semaan" /></div>
+      <div className="avatar">Camille Semaan <Avatar src={avatar} alt="Camille Semaan" /></div>
     </header>
   );
 }
